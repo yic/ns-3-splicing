64e6a8204c0a051c888999cd149914c6b25bacc1
Cheng Yi <yic@email.arizona.edu>
2012-01-08 17:53:55 -0700

    commit

---
+++ ns-3.12/evaluation/run.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+my $slices, $probability, $round;
+&print_usage;
+
+&run;
+
+sub print_usage {
+    if (@ARGV != 3) {
+        print STDERR "Usage: $0 slices probability round\n";
+        exit(-1);
+    }
+
+    $slices = $ARGV[0];
+    $probability = $ARGV[1];
+    $round = $ARGV[2];
+}
+
+sub run {
+    my $dir = "result-$slices-$probability";
+    mkdir($dir);
+    my $seed = time();
+
+    foreach my $i (0 .. $round - 1) {
+        chdir("..") or die($!);
+        system("./build/debug/scratch/sprint-path-splicing --FailureProbability=$probability --RngSeed=$seed > evaluation/$dir/result-$i-$seed 2>&1");
+        chdir("evaluation") or die($!);
+    }
+}
