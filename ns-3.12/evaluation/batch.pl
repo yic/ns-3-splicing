36f123ed60dbcad6106d789830bf8a2d9cbf93d0
Cheng Yi <yic@email.arizona.edu>
2012-01-10 11:18:37 -0700

    commit

---
+++ ns-3.12/evaluation/batch.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+my $slices, $retx, $min, $max, $round;
+&print_usage;
+
+my @probabilities = (0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1);
+
+foreach my $i ($min .. $max) {
+    system("./run.pl $slices $retx $probabilities[$i] $round");
+}
+
+sub print_usage {
+    if (@ARGV != 5) {
+        print STDERR "Usage: $0 slices retx min max round\n";
+        exit(-1);
+    }
+
+    $slices = $ARGV[0];
+    $retx = $ARGV[1];
+    $min = $ARGV[2];
+    $max = $ARGV[3];
+    $round = $ARGV[4];
+}
+
